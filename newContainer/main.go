5f7b03f44ce2681ae56e3dc92aac457e2e93d03c
Sneha Inguva <singuva@digitalocean.com>
2017-04-24 20:42:19 -0400

    container using namespaces

---
+++ newContainer/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("what?")
+	}
+}
+
+func run() {
+
+	// link to currently running process
+	// http://unix.stackexchange.com/questions/333225/which-process-is-proc-self-for
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...) // link to currently running process
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("running %v as pid%v\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	// TODO(si74) - Need to add own filesystem
+	//OPTION 1: get root filesystem, change directory path, and mount empty proc
+	must(syscall.Chroot("/home/rootfs"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
+	// OPTION 2: Mount root file system and pivot to root
+	// must(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
+	// must(os.MkdirAll("rootfs/oldrootfs", 0700))
+	// must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
+	// must(os.Chdir("/"))
+
+	must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
